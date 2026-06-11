9ede23dcf68d0579dee8c7f1362bf5a640656e3b
Anuskan-Panda <soyumpanda123@gmail.com>
2026-06-11 23:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.98%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int x=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(x!=nums[i])
+            {
+                return x;
+            }
+            x++;
+        }
+          return x;
+    }
+};
